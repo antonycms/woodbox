c37c94e74cbd2f6ddc8359965169e7580b0f0df4
Antony Santos <antonyc828@gmail.com>
2025-05-02 08:08:20 -0300

    lint

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,7 +1,7 @@
 import { resolve } from 'path';
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite';
 import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr'
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   main: {
@@ -16,6 +16,6 @@ export default defineConfig({
         '@renderer': resolve('src/renderer/src'),
       },
     },
-    plugins: [svgr(),react()],
+    plugins: [svgr(), react()],
   },
 });
