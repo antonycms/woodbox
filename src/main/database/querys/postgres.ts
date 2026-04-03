856594e09b3b28e877d77f482acde946f8dc51b5
Antony Santos <antonyc828@gmail.com>
2026-04-03 06:13:03 -0300

    fix: hidde internal postgres schemas

--- src/main/database/querys/postgres.ts
+++ src/main/database/querys/postgres.ts
@@ -6,7 +6,7 @@
 /* postgres only */
 const getAllSchemas = () => /* sql */ `
   SELECT schema_name FROM information_schema.schemata
-  WHERE schema_name NOT IN ('information_schema', 'pg_catalog', 'pg_toast')
+  WHERE schema_owner != 'postgres'
   ORDER BY schema_name;
 `;
 
