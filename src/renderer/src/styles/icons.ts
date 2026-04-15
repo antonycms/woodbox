0d7b5fca496006ed9b4efb3ff990e82567b5bea6
Antony Santos <antonyc828@gmail.com>
2026-04-15 19:08:00 -0300

    chore: add new Icon

--- src/renderer/src/styles/icons.ts
+++ src/renderer/src/styles/icons.ts
@@ -3,7 +3,7 @@ import { FiSave } from 'react-icons/fi';
 import { FaDatabase, FaFileAlt, FaFolderPlus, FaPlus, FaRegSun } from 'react-icons/fa';
 import { CgExport, CgTrash } from 'react-icons/cg';
 import { BiListPlus } from 'react-icons/bi';
-import { BsFiletypeSql } from 'react-icons/bs';
+import { BsFileEarmarkPlayFill, BsFiletypeSql } from 'react-icons/bs';
 import { makeIcon } from '@renderer/components/Icon';
 import {
   MdOutlineChromeReaderMode,
@@ -16,6 +16,7 @@ export const AddIcon = makeIcon(FaPlus, 20, 6);
 export const AddSqlIcon = makeIcon(BsFiletypeSql);
 export const SaveIcon = makeIcon(FiSave);
 export const RemoveIcon = makeIcon(CgTrash);
+export const RunFileIcon = makeIcon(BsFileEarmarkPlayFill);
 export const RunIcon = makeIcon(IoMdPlay);
 export const RunSelectionIcon = makeIcon(MdPlaylistPlay);
 export const DuplicateIcon = makeIcon(BiListPlus);
