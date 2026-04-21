77d667de400bb38998b2992576a45f791c5a7dd9
Antony Santos <antonyc828@gmail.com>
2026-04-20 21:39:48 -0300

    feat: add tab context menu with close options to MainContent

--- src/renderer/src/components/MainContent/index.tsx
+++ src/renderer/src/components/MainContent/index.tsx
@@ -1,15 +1,56 @@
 import React from 'react';
-import { TabBar, TabWindow, TabContent } from '@renderer/components/Tabs';
+import { TabBar, TabWindow, TabContent, IActiveTabContextMenu } from '@renderer/components/Tabs';
 import { Welcolme } from '@renderer/components/Welcome';
 import { useAppTabContext } from '@renderer/contexts/AppTab';
 import { useThemeContext } from '@renderer/contexts/Theme';
 import styles from './styles.module.css';
+import { IContextMenuOption } from '../ContextMenu';
 
 export const MainContent = () => {
   const { tabs, removeTab, activeTabId, setActiveTabId } = useAppTabContext();
   const {
     activeTheme: { mainTab: theme },
   } = useThemeContext();
+
+  const contextMenuOptions = React.useMemo<IContextMenuOption<IActiveTabContextMenu>[]>(
+    () => [
+      {
+        text: 'Fechar aba',
+        onClick: (info) => {
+          setActiveTabId(info.tab.idTab);
+          tabs.forEach((t) => t.id === info.tab.idTab && removeTab(t.id));
+        },
+      },
+      tabs.length > 1 && {
+        text: 'Fechar outras abas',
+        onClick: (info) => {
+          setActiveTabId(info.tab.idTab);
+          tabs.forEach((t) => t.id !== info.tab.idTab && removeTab(t.id));
+        },
+      },
+      tabs.length > 1 && {
+        text: 'Fechar abas à esquerda',
+        onClick: (info) => {
+          const idx = tabs.findIndex((t) => t.id === info.tab.idTab);
+          tabs.slice(0, idx).forEach((t) => removeTab(t.id));
+        },
+      },
+      tabs.length > 1 && {
+        text: 'Fechar abas à direita',
+        onClick: (info) => {
+          const idx = tabs.findIndex((t) => t.id === info.tab.idTab);
+          tabs.slice(idx + 1).forEach((t) => removeTab(t.id));
+        },
+      },
+      tabs.length > 1 && {
+        text: 'Fechar todas as abas',
+        onClick: () => {
+          tabs.forEach((t) => removeTab(t.id));
+        },
+      },
+    ],
+    [tabs],
+  );
 
   if (!tabs.length) return <Welcolme />;
 
@@ -29,6 +70,7 @@ export const MainContent = () => {
         idTabBar="app_tabs"
         onRemoveTab={(tab) => removeTab(tab.idTab)}
         tabs={tabs.map(({ id: idTab, title, unsaved }) => ({ idTab, title, unsaved }))}
+        contextMenuOptions={contextMenuOptions}
       />
 
       <TabWindow idTabBar="app_tabs">
