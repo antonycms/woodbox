b0d9a42290e4ac0eea12231779719040fc5aad4f
Antony Santos <antonyc828@gmail.com>
2026-04-25 19:35:59 -0300

    refactor(IconItemTreeView): migrate all icons to unplugin-icons with size wrapper helper

--- src/renderer/src/components/TreeView/IconItemTreeView/index.tsx
+++ src/renderer/src/components/TreeView/IconItemTreeView/index.tsx
@@ -1,35 +1,45 @@
 import React from 'react';
 import { classes } from '@renderer/styles/theme';
-import { FaDatabase, FaFolder } from 'react-icons/fa';
-import {
-  MdFunctions,
-  MdGridView,
-  MdKeyboardArrowDown,
-  MdKeyboardArrowRight,
-  MdOutlineSchema,
-} from 'react-icons/md';
-import { FaFolderTree } from 'react-icons/fa6';
-import { BsFiletypeSql, BsFileEarmarkText } from 'react-icons/bs';
-import { TbBoxMultipleFilled } from 'react-icons/tb';
-import { PiFunctionLight } from 'react-icons/pi';
-import { AiOutlineTable } from 'react-icons/ai';
 import styles from './styles.module.css';
 
+import IconMdiKeyboardArrowDown from '~icons/mdi/keyboard-arrow-down';
+import IconMdiKeyboardArrowRight from '~icons/mdi/keyboard-arrow-right';
+import IconMdiViewGridOutline from '~icons/mdi/view-grid-outline';
+import IconMdiFunction from '~icons/mdi/function';
+import IconMdiFileTree from '~icons/mdi/file-tree-outline';
+import IconMdiSigma from '~icons/mdi/sigma';
+import IconFaSolidDatabase from '~icons/fa-solid/database';
+import IconFaSolidFolder from '~icons/fa-solid/folder';
+import IconFaSolidFolderTree from '~icons/fa6-solid/folder-tree';
+import IconBiFiletypeSql from '~icons/bi/filetype-sql';
+import IconBiFileEarmarkText from '~icons/bi/file-earmark-text';
+import IconTableBoxMultipleFilled from '~icons/tabler/box-multiple-filled';
+import IconAntDesign from '~icons/ant-design/table-outlined';
+
+function makeIconWithSize(
+  Icon: React.ForwardRefExoticComponent<React.SVGProps<SVGSVGElement>>,
+  size: number,
+) {
+  return (props: React.SVGProps<SVGSVGElement>) => (
+    <Icon {...props} width={props?.width || size} height={props?.height || size} />
+  );
+}
+
 const availableIcons = {
-  default: FaFolder,
-  folder: FaFolder,
-  file: BsFileEarmarkText,
-  database: FaDatabase,
-  table: AiOutlineTable,
-  arrowDown: MdKeyboardArrowDown,
-  arrowRight: MdKeyboardArrowRight,
-  schema: MdOutlineSchema,
-  fileSql: BsFiletypeSql,
-  grid: MdGridView,
-  folderTree: FaFolderTree,
-  functions: MdFunctions,
-  multi: TbBoxMultipleFilled,
-  function: PiFunctionLight,
+  default: makeIconWithSize(IconFaSolidFolder, 16),
+  folder: makeIconWithSize(IconFaSolidFolder, 16),
+  file: makeIconWithSize(IconBiFileEarmarkText, 16),
+  database: makeIconWithSize(IconFaSolidDatabase, 16),
+  table: makeIconWithSize(IconAntDesign, 16),
+  arrowDown: makeIconWithSize(IconMdiKeyboardArrowDown, 16),
+  arrowRight: makeIconWithSize(IconMdiKeyboardArrowRight, 16),
+  schema: makeIconWithSize(IconMdiFileTree, 16),
+  fileSql: makeIconWithSize(IconBiFiletypeSql, 16),
+  grid: makeIconWithSize(IconMdiViewGridOutline, 16),
+  folderTree: makeIconWithSize(IconFaSolidFolderTree, 16),
+  functions: makeIconWithSize(IconMdiSigma, 16),
+  multi: makeIconWithSize(IconTableBoxMultipleFilled, 16),
+  function: makeIconWithSize(IconMdiFunction, 16),
 };
 
 const IconItemTreeView = ({ no_margin, icon = 'folder', ...props }: IIcon) => {
