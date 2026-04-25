08ad5df496da0507bb8866304b8094161db643ae
Antony Santos <antonyc828@gmail.com>
2026-04-25 19:35:51 -0300

    feat(icons): add unplugin-icons type declarations and replace icons.ts with icons.tsx

---
+++ src/renderer/src/@types/unplugin.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="unplugin-icons/types/react" />
+
+declare module '~icons/*' {
+  import type { ComponentType, SVGProps } from 'react';
+  const component: ComponentType<SVGProps<SVGSVGElement>>;
+  export default component;
+}
---
+++ src/renderer/src/styles/icons.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import IconMdiFileDocumentOutline from '~icons/mdi/file-document-outline';
+import IconMdiPlaylistPlay from '~icons/mdi/playlist-play';
+import IconMdiBookOpenOutline from '~icons/mdi/book-open-outline';
+import IconMdiRefresh from '~icons/mdi/refresh';
+import IconFaSolidPlus from '~icons/fa-solid/plus';
+import IconFaSolidCog from '~icons/fa-solid/cog';
+import IconFaSolidFolder from '~icons/fa-solid/folder';
+import IconFaSolidFolderPlus from '~icons/fa-solid/folder-plus';
+import IconFaSolidDatabase from '~icons/fa-solid/database';
+import IconFaSolidFileAlt from '~icons/fa-solid/file-alt';
+import IconFeatherSave from '~icons/feather/save';
+import IconBiFiletypeSql from '~icons/bi/filetype-sql';
+import IconBiFileEarmarkPlayFill from '~icons/bi/file-earmark-play-fill';
+import IconGgTrash from '~icons/gg/trash';
+import IconGgExport from '~icons/gg/export';
+import IconBxListPlus from '~icons/bx/list-plus';
+import IconIonMdPlay from '~icons/ion/md-play';
+
+function makeIconWithSize(
+  Icon: React.ForwardRefExoticComponent<React.SVGProps<SVGSVGElement>>,
+  size = 20,
+) {
+  return (props: React.SVGProps<SVGSVGElement> & { title?: string; size?: number }) => (
+    <Icon
+      {...props}
+      width={props?.width ?? props?.size ?? size}
+      height={props?.height ?? props?.size ?? size}
+    />
+  );
+}
+
+export const AddIcon = makeIconWithSize(IconFaSolidPlus);
+export const FileSqlIcon = makeIconWithSize(IconBiFiletypeSql);
+export const SaveIcon = makeIconWithSize(IconFeatherSave);
+export const RemoveIcon = makeIconWithSize(IconGgTrash);
+export const RunFileIcon = makeIconWithSize(IconBiFileEarmarkPlayFill);
+export const RunIcon = makeIconWithSize(IconIonMdPlay);
+export const RunSelectionIcon = makeIconWithSize(IconMdiPlaylistPlay);
+export const DuplicateIcon = makeIconWithSize(IconBxListPlus);
+export const ExportIcon = makeIconWithSize(IconGgExport);
+export const IconDatabase = makeIconWithSize(IconFaSolidDatabase);
+export const IconSettings = makeIconWithSize(IconFaSolidCog);
+export const IconFolder = makeIconWithSize(IconFaSolidFolder);
+export const IconFolderAdd = makeIconWithSize(IconFaSolidFolderPlus);
+export const IconFile = makeIconWithSize(IconFaSolidFileAlt);
+export const PanelFile = makeIconWithSize(IconMdiBookOpenOutline);
+export const IconFileWrited = makeIconWithSize(IconMdiFileDocumentOutline);
+export const IconRefresh = makeIconWithSize(IconMdiRefresh);
