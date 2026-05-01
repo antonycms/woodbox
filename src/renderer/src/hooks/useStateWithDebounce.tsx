029b0f47759492b87de7173264572fe9ab7efd13
Antony Santos <antonyc828@gmail.com>
2026-05-01 19:45:30 -0300

    chore: improve hooke useStateWithDebounce

--- src/renderer/src/hooks/useStateWithDebounce.tsx
+++ src/renderer/src/hooks/useStateWithDebounce.tsx
@@ -2,16 +2,12 @@
 import { useState } from 'react';
 import useDebounce from './useDebounce';
 
-type ValueOrCallback<Type> = Type | (() => Type);
+function useStateWithDebounce<Type = any>(initialValue: Type, delay?: number) {
+  const [state, setState] = useState<Type>(initialValue);
 
-function useStateWithDebounce<Type = any>(valueOrCallback?: ValueOrCallback<Type>, delay?: number) {
-  const [state, setState] = useState<Type>(valueOrCallback);
-  const setStateWithDebounce = useDebounce<React.Dispatch<React.SetStateAction<Type>>>(
-    setState,
-    delay,
-  );
+  const setDebounce = useDebounce<React.Dispatch<React.SetStateAction<Type>>>(setState, delay);
 
-  return [state, setStateWithDebounce] as const;
+  return [state, setDebounce] as const;
 }
 
 export default useStateWithDebounce;
