add5c636aeb7b04972e544c6a7ea67fd654518c1
Antony Santos <antonyc828@gmail.com>
2026-07-26 20:06:59 -0300

    fix(editor): register Monaco services for contributions

--- src/renderer/src/components/Editor/monaco.ts
+++ src/renderer/src/components/Editor/monaco.ts
@@ -3,6 +3,8 @@ import * as monaco from 'monaco-editor/editor/editor.api';
 import 'monaco-editor/languages/definitions/sql/register';
 import 'monaco-editor/languages/features/json/register';
 
+import 'monaco-editor/editor/common/services/treeViewsDndService';
+import 'monaco-editor/editor/contrib/codelens/browser/codeLensCache';
 import 'monaco-editor/editor/contrib/bracketMatching/browser/bracketMatching';
 import 'monaco-editor/editor/contrib/find/browser/findController';
 import 'monaco-editor/editor/contrib/folding/browser/folding';
