b932b9b9ccc604dbbd3237078882bfcbb90820a9
Antony Santos <antonyc828@gmail.com>
2026-07-27 22:20:02 -0300

    feat(editor): load all Monaco editor features

--- src/renderer/src/components/Editor/monaco.ts
+++ src/renderer/src/components/Editor/monaco.ts
@@ -1,17 +1,8 @@
 import * as monaco from 'monaco-editor/editor/editor.api';
 
+import 'monaco-editor/features/register.all';
 import 'monaco-editor/languages/definitions/sql/register';
 import 'monaco-editor/languages/features/json/register';
-
-import 'monaco-editor/editor/common/services/treeViewsDndService';
-import 'monaco-editor/editor/contrib/codelens/browser/codeLensCache';
-import 'monaco-editor/editor/contrib/bracketMatching/browser/bracketMatching';
-import 'monaco-editor/editor/contrib/find/browser/findController';
-import 'monaco-editor/editor/contrib/folding/browser/folding';
-import 'monaco-editor/editor/contrib/hover/browser/hoverContribution';
-import 'monaco-editor/editor/contrib/suggest/browser/suggestController';
-import 'monaco-editor/editor/contrib/wordOperations/browser/wordOperations';
-import 'monaco-editor/editor/contrib/tokenization/browser/tokenization';
 
 export * from 'monaco-editor/editor/editor.api';
 export default monaco;
