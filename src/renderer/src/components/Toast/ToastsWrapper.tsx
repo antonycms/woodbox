19b9b47cb2f61b352a2a46eb1e5d3ff1206e4455
Antony Santos <antonyc828@gmail.com>
2026-08-10 20:43:05 -0300

    fix(layout): adjust panel layout details

--- src/renderer/src/components/Toast/ToastsWrapper.tsx
+++ src/renderer/src/components/Toast/ToastsWrapper.tsx
@@ -2,5 +2,9 @@ import React from 'react';
 import styles from './styles.module.css';
 
 export const ToastsWrapper = ({ children }) => {
+  const hasChildren = React.Children.toArray(children).length;
+
+  if (!hasChildren) return null;
+  
   return <div className={styles.toastsWrapper}>{children}</div>;
 };
